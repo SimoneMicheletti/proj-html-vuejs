ba30d69cdd54e2d414590c59eb0554b1df81557d
SimoneMicheletti <sm.micheletti@gmail.com>
2020-12-11 15:51:39 +0100

    Sezioni Team e Jumbo dinamiche con Vue

--- js/script.js
+++ js/script.js
@@ -2,23 +2,83 @@ var app = new Vue ({
   el: "#app",
   data: {
 
+    // Valore per mostrare comunicazioni all'utente
     comunication: { tf: false,
                     type: ""
                   },
 
-    company:  { openHours: "Mon - Sat - 9:00 - 18:00",
+    // Inserire qui i dati della propria azienda
+    company:  { name1: "N e x",
+                name2: "G e n",
+                sector: "Insurance Consulting",
+                focus: "Fusion & Acquisitions",
+                openHours: "Mon - Sat - 9:00 - 18:00",
                 phoneNum: "+1 (305) 1234-5678",
                 email: "hello@example.com",
                 address: "Main Avenue, 987",
                 description: "A Functional HTML Template for Corporate & Business."
               },
 
+    // Inserire qui i dati dei membri dello staf che si vogliono mostrare
+    team: [ { name: "David Cooper",
+              img: "img/team-1.jpg",
+              position: "CTO & CO-FOUNDER",
+              linkFb: "#team",
+              linkTw: "#team",
+              linkIn: "#team"
+            },
+            { name: "Oliver Jones",
+              img: "img/team-3.jpg",
+              position: "CHIEF PROCUREMENT",
+              linkFb: "#team",
+              linkTw: "#team",
+              linkIn: "#team"
+            },
+            { name: "Emma Lopez",
+              img: "img/team-2.jpg",
+              position: "CHIEF MARKETING",
+              linkFb: "#team",
+              linkTw: "#team",
+              linkIn: "#team"
+            },
+            { name: "T. Johnson",
+              img: "img/team-4.jpg",
+              position: "CEO & PRESIDENT",
+              linkFb: "#team",
+              linkTw: "#team",
+              linkIn: "#team"
+            },
+          ],
+
+    // Qui verranno registrati i dati dell'utente
     user: { name: "",
             email: "",
             phoneNum: "",
             type: ""
           },
 
+    // Struttura della HomePage
+    homeStruct: [ { obj: "HOME",
+                    link: "index.html"
+                  },
+                  { obj: "ABOUT",
+                    link: "#company"
+                  },
+                  { obj: "SERVICES",
+                    link: "#services"
+                  },
+                  { obj: "PROCESS",
+                    link: "#process"
+                  },
+                  { obj: "TEAM",
+                    link: "#team"
+                  },
+                  { obj: "BLOG",
+                    link: "index.html"
+                  },
+                ],
+
+    // Struttura dell'intero sito web
     wStruct:[ { title: "About",
                 list: [ { obj: "The Company",
                           link: "#company"
@@ -95,7 +155,6 @@ var app = new Vue ({
       } else {
         alert("Please fill in both forms")
       }
-      this.closeLink = "#know"
     },
 
     // Funzione Contattaci
